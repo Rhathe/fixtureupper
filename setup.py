7b8f76d53fb820e4fd429f880557bf52ec5dc4be
Rhathe <rassinator@gmail.com>
2016-10-21 17:57:26 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='fixture-generator',
+    version='0.0.1',
+    packages=['fixture-generator'],
+    url='https://github.com/Rhathe/Fixture-Generator',
+    license='MIT',
+    author='Ramon Sandoval',
+    description='SqlAlchemy Fixture Generator',
+    long_description='SqlAlchemy Fixture Generator',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ],
+
+    install_requires=[
+    ],
+)
