9bfd6fa8f241bf700d4c01094ff40949290dfbf0
Rhathe <rassinator@gmail.com>
2016-11-23 14:47:53 -0500

    Add more tests

--- tests/unit/test_model_fixture_upper.py
+++ tests/unit/test_model_fixture_upper.py
@@ -2,13 +2,27 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from future.utils import iteritems
 
+from mock import Mock
 from unittest import TestCase
 
 from fixtureupper.register import UpperRegister
 
 class TestModelFixtureUpper(TestCase):
     def setUp(self):
-        self.ModelFixtureUpper = UpperRegister('Model')
+        class ModelFixtureUpper(UpperRegister('Model')):
+            model = Mock()
+
+            def get_model_attr_key(self, *args, **kwargs):
+                return 'attr_key'
+
+            def set_relation(self, fixture, related_fixtures, relation_prop):
+                setattr(fixture, relation_prop, 'related value: %s' % related_fixtures)
+
+            @classmethod
+            def get_relationships(cls):
+                return {'rel_1': Mock(), 'rel_2': Mock(), 'rel_3': Mock()}
+
+        self.ModelFixtureUpper = ModelFixtureUpper
 
     def test_sorted_by_generated_order(self):
         self.ModelFixtureUpper.generated_field_order = ['a', 'z', 'c', 'b', 'd']
@@ -55,3 +69,35 @@ class TestModelFixtureUpper(TestCase):
         b = TestNotInOrderModelFixture()
         self.assertEqual(self.ModelFixtureUpper.sorted_fixtures_key(a), '0002_TestModelFixture')
         self.assertEqual(self.ModelFixtureUpper.sorted_fixtures_key(b), '0004_TestNotInOrderModelFixture')
+
+    def test_set_fixture_values(self):
+        fixture = Mock(rel_1=None, rel_2=None, static_2=None, static_3=None)
+        m_fu = self.ModelFixtureUpper()
+        fixture2 = m_fu.set_fixture_values({
+            'rel_1': lambda *args: 5,
+            'rel_3': 10,
+            'static_1': 20,
+            'static_3': lambda *args: 4,
+        }, fixture=fixture)
+
+        self.assertEqual(fixture, fixture2)
+        self.assertEqual(fixture.rel_1, 'related value: 5')
+        self.assertEqual(fixture.rel_2, None)
+        self.assertEqual(fixture.rel_3, 'related value: 10')
+        self.assertEqual(fixture.static_1, 20)
+        self.assertEqual(fixture.static_2, None)
+        self.assertEqual(fixture.static_3, 4)
+
+    def test_set_fixture_values_creates_fixture_if_None(self):
+        m_fu = self.ModelFixtureUpper()
+        fixture = m_fu.set_fixture_values({
+            'rel_1': lambda *args: 5,
+            'rel_3': 10,
+            'static_1': 20,
+            'static_3': lambda *args: 4,
+        })
+
+        self.assertEqual(fixture.rel_1, 'related value: 5')
+        self.assertEqual(fixture.rel_3, 'related value: 10')
+        self.assertEqual(fixture.static_1, 20)
+        self.assertEqual(fixture.static_3, 4)
