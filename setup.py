20ca16908d18a8969327a4575e06802c3d1d25c2
Christopher Sang <csdev@users.noreply.github.com>
2018-05-03 16:24:03 -0400

    Bump version to 0.2.10

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class ToxWithRecreate(Tox):
 
 setup(
     name='fixtureupper',
-    version='0.2.9',
+    version='0.2.10',
     packages=['fixtureupper'],
     url='https://github.com/Rhathe/fixtureupper',
     license='MIT',
